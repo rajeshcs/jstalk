fed7d2b0c6e2a1be2312c1bdef67156b3456774c
Rajesh Kumar <rajesh@rajeshs-mbp.att.net>
2014-10-19 15:02:06 -0700

    Updated slides

--- jsCode/array.js
+++ jsCode/array.js
@@ -1,5 +1,10 @@
-var myArray = [];
-print(myArray.length);
+var myArr = []
+myArr[0] = "hi" 
+myArr[1] = "there"
 
-myArray[100] = true;
-print(myArray.length);
+
+var myArr1 = new Array(40, 100)
+print(myArr1.length)
+
+var myArr2 = new Array(40)
+print(myArr2.length)
